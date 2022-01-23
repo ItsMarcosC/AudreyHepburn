7a1a3ad11f91fc400a675a137f02450303ef64a6
Marcos Carvalho <marcosfilhosp@hotmail.com>
2022-01-22 21:19:45 -0300

    Implements Gravatar on Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
 
-function Header({displayName}) {
+function Header({displayName, gravatarEmail}) {
+
+  const getProfileImage = () => {
+    const hash = md5(gravatarEmail).toString();
+    return `https://www.gravatar.com/avatar/${hash}`;
+  }
+
   switch (displayName) {
     case "":
       return (
@@ -13,14 +20,15 @@ function Header({displayName}) {
       return (
         <div>
           <h2>{`Hello ${displayName} `}</h2>
+          <img src={getProfileImage()} alt='' />
         </div>
       );
   }
-  
 }
 
 const mapStateToProps = (state) => ({
   displayName: state.login.name,
+  gravatarEmail: state.login.email,
 });
 
 export default connect(mapStateToProps)(Header);
